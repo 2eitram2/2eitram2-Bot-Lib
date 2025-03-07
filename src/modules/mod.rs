61ad81a86e698f71493c323cbacc807395593eda
2eitram2 <etienne.beard63000@gmail.com>
2025-03-07 17:37:39 +0100

    Delete src directory

--- src/modules/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod keys;
-pub mod tcp;
-pub mod utils;
-pub mod handle;
-pub mod encryption;
-pub mod client;
--- src/modules/utils.rs
+++
@@ -1,8 +0,0 @@
-use sha2::{Digest, Sha256};
-use hex;
-
-pub fn sha256_hash(input: &[u8]) -> String {
-    let mut hasher = Sha256::new();
-    hasher.update(input);
-    hex::encode(hasher.finalize())
-}
